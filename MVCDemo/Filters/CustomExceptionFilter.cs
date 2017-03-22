d88af771e8d49faa9c548ca2e2a68dfce7a9c4b7
Jimmy Wang <hltzba@DESKTOP-HOME>
2017-03-22 23:16:24 +0800

    新增自定义异常过滤器

---
+++ MVCDemo/Filters/CustomExceptionFilter.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCDemo.Filters
+{
+    public class CustomExceptionFilter : HandleErrorAttribute
+    {
+        public override void OnException(ExceptionContext filterContext)
+        {
+            //TODO WriteLog
+            base.OnException(filterContext);
+        }
+    }
+}
