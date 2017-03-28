5116928b8644cadafb170ef4526e40dc09eef3dc
JimmyWang-Desktop <hltzba@163.com>
2017-03-28 17:29:56 +0800

    单页应用部分提交

---
+++ MVCDemo/Areas/SPA/Controllers/MainController.cs
@@ -0,0 +1,54 @@
+﻿using DAL;
+using DAL.BussinessLayer;
+using MVCDemo.ViewModel.SPA;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCDemo.Areas.SPA.Controllers
+{
+    public class MainController : Controller
+    {
+        // GET: SPA/Main
+        public ActionResult Index()
+        {
+            List<Employee> employees = EmployeesDAL.GetAllEmployees();
+            List<EmployeeViewModel> resultModels = new List<EmployeeViewModel>();
+            foreach (Employee e in employees)
+            {
+                EmployeeViewModel model = new EmployeeViewModel();
+                model.EmployeeName = e.FirstName + " " + e.LastName;
+                model.Salary = e.Salary;
+                if (model.Salary > 2000)
+                {
+                    model.SalaryColor = "green";
+                }
+                else
+                {
+                    model.SalaryColor = "yellow";
+                }
+            }
+            return View();
+        }
+
+        public ActionResult GetAddNewLink()
+        {
+            if (Convert.ToBoolean(Session["IsAdmin"]))
+            {
+                return PartialView("GetAddNewLink");
+            }
+            else
+            {
+                return new EmptyResult();
+            }
+        }
+
+        public ActionResult AddNew()
+        {
+            CreateEmployeeViewModel v = new CreateEmployeeViewModel();
+            return PartialView("CreateEmployee", v);
+        }
+    }
+}
---
+++ ViewModel/SPA/EmployeeViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MVCDemo.ViewModel.SPA
+{
+    public class EmployeeViewModel
+    {
+        public string EmployeeName
+        { get; set; }
+
+        public int Salary
+        { get; set; }
+
+        public string SalaryColor
+        { get; set; }
+    }
+}
